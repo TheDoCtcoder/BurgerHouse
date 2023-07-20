9f1631d5c3ff83285b7a4523c38f8d38c51ed545
Toni Deplano <montana.toni@gmail.com>
2023-07-21 00:44:19 +0200

    Burger House change RouterBrowser to HashRouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Route, Switch, BrowserRouter } from 'react-router-dom'
+// import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import { Route, Switch, HashRouter } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
       <Route exact path='/'>
             <Homepage />
         </Route>
       </Switch>
-      </BrowserRouter>
+      </HashRouter>
   )
 }
 
