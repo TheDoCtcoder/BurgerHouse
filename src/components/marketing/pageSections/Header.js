558bb2d01f6dc16b3b95a7bcd24a6058b2344910
Toni Deplano <montana.toni@gmail.com>
2023-07-23 20:03:53 +0200

    Final Version

--- src/components/marketing/pageSections/Header.js
+++ src/components/marketing/pageSections/Header.js
@@ -10,9 +10,10 @@ export default function Header() {
                 <img src={Logo} alt='Burger House App' className='w-64' />
             </div>
             <div className=' text-secondary w-full   '>
+         
                 <div className='flex items-center justify-end'>
                 <img src={Icone} alt='' className='w-5 h-5 mr-1'></img>
-                <span>Commandez votre repas en ligne</span>
+                <span className=' font-bold tracking-wide text-lg' >Commandez votre repas en ligne</span>
                 </div>
                 <div className="mt-5 flex items-center justify-end  ">
                     <Bouton className='mr-3 tracking-widest '>
